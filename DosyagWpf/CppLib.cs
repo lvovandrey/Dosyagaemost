a1fe3ed7a24a83738192faa631a439ac679dbd9d
Lvov Andrey <lvovandrey@mail.ru>
2019-04-19 16:53:03 +0300

    Переделал dll под х64

--- DosyagWpf/CppLib.cs
+++ DosyagWpf/CppLib.cs
@@ -9,33 +9,34 @@ namespace DosyagWpf
 {
     class CppLib
     {
+        const string dll = "DosyagDll64.dll";
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double Dosyagaemost([In]int FDtype, [In]int OUtype, [In]bool Spec, [In]double x, [In]double y, [In]double z);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double alpha([In]double x, [In]double y, [In]double z);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double dist([In]double x, [In]double y, [In]double z);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double h([In]double x, [In]double y, [In]double z);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double x([In]double alpha, [In]double dist, [In]double height);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double y([In]double alpha, [In]double dist, [In]double height);
 
         [return: MarshalAs(UnmanagedType.R8)]
-        [DllImport("DosyagDll.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern double z([In]double alpha, [In]double dist, [In]double height);
 
     }
